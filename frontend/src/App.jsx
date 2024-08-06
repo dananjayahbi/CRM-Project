b83fd39715927e58e2046b358921f421170c490b
djd <dananjayahbi@gmail.com>
2024-08-06 12:59:42 +0530

    feat: Add mobile login page and update login functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import LoginMobile from "./pages/LoginMobile";
 import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   const isLogged = window.sessionStorage.getItem("LoggedIn");
 
-  //-------------------------------------
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1300);
 
-  //clear local storage
-  // window.sessionStorage.clear();
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1300);
+    };
 
-  //force login
-  // window.sessionStorage.setItem("LoggedIn", true);
-
-  //-------------------------------------
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <Routes>
@@ -27,7 +29,7 @@ const App = () => {
           <Route path="*" element={<Navigate to="/login" />} />
         </>
       )}
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={isMobile ? <LoginMobile /> : <Login />} />
     </Routes>
   );
 };
